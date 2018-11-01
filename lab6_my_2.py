2f898dbe897631ef32e729fa9060e69077ae5785
iCAROS7 <hominlab@gmail.com>
2018-11-01 23:14:15 +0900

    Assignment No. 14 #2
    + define all comparison operators magic method
    + fixed a bug that was not reduce fraction in some case

--- lab6_my_2.py
+++ lab6_my_2.py
@@ -85,11 +85,11 @@ class Fraction:
         :param o: self에 곱할 분수
         :return: 곱한 결과 값을 반환
         """
-        tong = gcd(self.denom, o.denom)  # 최대공약수 값을 구해 저장
         # 결과의 분모 계산
         d = self.denom * o.denom
         # 결과의 분자 계산
         n = self.numer * o.numer
+        tong = gcd(n, d)  # 곱셈의 경우 분자와 분모의 최대 공략수를 구해 저장
         if (n % tong == 0) and (d % tong == 0):  # 통분해야할 것이 있다면
             r = Fraction(int(n / tong), int(d / tong))  # 결과값을 통분 한 뒤 해당 값으로 Fraction 분수 생성
         else:
@@ -115,40 +115,85 @@ class Fraction:
 
     def __str__(self):
         """
+        # 이쁘게 보여주기 위한 음수 괄호 묶음
         if (self.numer < 0) and (self.denom > 0):
             return "-(" + str(abs(self.numer)) + "/" + str(self.denom) + ")"
         elif (self.numer > 0) and (self.denom < 0):
             return "-(" + str(self.numer) + "/" + str(abs(self.denom)) + ")"
         elif (self.numer < 0) and (self.denom < 0):
             return "-(" + str(abs(self.numer)) + "/" + str(abs(self.denom)) + ")"
-        else:"""
+        else:
+        """
+        # 음수 괄호 묶지 않은채 각개 출력
         return "(" + str(self.numer) + "/" + str(self.denom) + ")"
 
-    def __eq__(self, other):
-        """
-        통분 해야하는가?
-        """
-        g1 = gcd(self.numer, self.denom)
-        g2 = gcd(other.numer, other.denom)
-        if (self.numer//g1 == other.numer//g2) and (self.denom//g1 == other.denom//g2):
+    def __eq__(self, o):  # == 연산자 계산
+        n1 = self.numer * o.denom
+        n2 = o.numer * self.denom
+        if n1 == n2:
             return True
         else:
             return False
 
-    def __ne__(self, other):
-        """
-        두 분수가 같으면 참, 다르면 거짓
-        """
-        if self == other:
+    def __lt__(self, o):  # < 연산자 계산
+        n1 = self.numer * o.denom
+        n2 = o.numer * self.denom
+        if n1 < n2:
+            return True
+        else:
+            return False
+
+    def __le__(self, o):  # <= 연산자 계산
+        n1 = self.numer * o.denom
+        n2 = o.numer * self.denom
+        if n1 <= n2:
+            return True
+        else:
+            return False
+
+    def __ne__(self, o):  # != 연산자 계산
+        n1 = self.numer * o.denom
+        n2 = o.numer * self.denom
+        if n1 != n2:
+            return True
+        else:
+            return False
+
+    def __ge__(self, o):  # >= 연산자 계산
+        n1 = self.numer * o.denom
+        n2 = o.numer * self.denom
+        if n1 >= n2:
+            return True
+        else:
+            return False
+
+    def __gt__(self, o):  # > 연산자 계산
+        n1 = self.numer * o.denom
+        n2 = o.numer * self.denom
+        if n1 > n2:
             return True
         else:
             return False
 
 
-f1 = Fraction(-1, -2)  # Fraction 인스턴스 생성
-f2 = Fraction(-5, -4)  # Fraction 인스턴스 생성
+# 조건에서 주어진 각 분수 생성
+f1_1 = Fraction(-4, 5)
+f1_2 = Fraction(3, 2)
 
-print(f1, "+", f2, "=", f1 + f2)
-print(f1, "-", f2, "=", f1 - f2)
-print(f1, "*", f2, "=", f1 * f2)
-print(f1, "/", f2, "=", f1 / f2)
+f2_1 = Fraction(8, 31)
+f2_2 = Fraction(2, 3)
+
+f3_1 = Fraction(3, 5)
+f3_2 = Fraction(3, 7)
+
+f4_1 = Fraction(4, 8)
+f4_2 = Fraction(1, 2)
+
+f5_1 = Fraction(2, 81)
+f5_2 = Fraction(3, 9)
+
+print(f1_1 * f1_2)
+print(f2_1 - f2_2)
+print(f3_1 + f3_2)
+print(f4_1 == f4_2)
+print(f5_1 >= f5_2)
